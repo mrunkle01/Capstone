335474d6abcaf9849f4d8bc5b365bb5861bcda47
Mark Runkle <markrunkle67@gmail.com>
2026-02-23 15:46:43 -0600

    Displayed Image correctly

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
      <Link href="/login">Go to login page</Link>
     </div>
   );
