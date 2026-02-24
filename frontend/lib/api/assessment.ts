77e55b9a824b15c0c1352cc0ce21e881f8b6e89b
Mark Runkle <markrunkle67@gmail.com>
2026-02-24 14:35:45 -0600

    test2

--- frontend/lib/api/assessment.ts
+++ frontend/lib/api/assessment.ts
@@ -1,6 +1,6 @@
 export async function assessImage(formData: FormData){
     const section_id = 0 //made this hard coded for the demo
-    const url = `${process.env.NEXT_PUBLIC_API_URL}/api/assess/${section_id}`
+    const url = `https://capstone-production-6abe.up.railway.app/api/assess/${section_id}`
     const response = await fetch(url, {
         method: "POST",
         body: formData,
