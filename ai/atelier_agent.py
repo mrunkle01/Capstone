5b90c1e3cac5f1e1497fddbc64884523d73b2687
21stRegi <115488242+scherry985@users.noreply.github.com>
2026-03-24 13:47:49 -0500

    Separated instructions from code
    The system will now retrieve instructions from text files to prompt the AI with.

--- ai/atelier_agent.py
+++ ai/atelier_agent.py
@@ -1,9 +1,11 @@
 import base64
 import os
-import ollama
+from pydantic_core import ValidationError
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from ollama import Client, WebSearchResponse, WebFetchResponse
+
+# Atelier Agent Properties
 
 personality = [
         {'role': 'system', 'content': """You are a critical but supportive assistant in personal growth and 
@@ -19,6 +21,39 @@ options = {
     'temperature': 0.8
 }
 
+
+# Atelier Agent Methods
+def get_personality():
+    temp = []
+    for value in personality:
+        temp.append(value)
+    return temp
+
+
+def chat(prompt):
+    pass
+    # Get user info and retrieve memory
+
+    # Web search
+
+    # # Handle Tool Calls
+    # info = []
+    # for call in response.message.tool_calls:
+    #     func = tools.get(call.function.name)
+    #     if func:
+    #         # Try Catch
+    #         result = func(**call.function.arguments)
+    #         info.append({'role': 'tool', 'tool_name': call.function.name, 'content': str(result)})
+    #
+    # prompt += info
+    #
+    # informed_response = con.client.chat(
+    #     model,
+    #     messages=prompt, format=LessonPlan.model_json_schema(),
+    #     tools=[get_user_materials],
+    #     think=True, options=options)
+
+    # Do error checking and retry
 
 class AtelierConnection():
     client: Client
@@ -86,28 +121,21 @@ def grade_art(assignment: str, img: bytes) -> Grade:
     con = AtelierConnection()
     model = 'qwen3-vl:235b-cloud'
 
-    #user_info = con.fetch_user_info()
-
     img_final = base64.b64encode(img).decode()
 
-    messages = [
-        {'role': 'user',
-         'content': "Can you grade my performance drawing this image according to the following assignment",
-         'images': [img_final]},
-        {'role': 'user', 'content': assignment},
-        {'role': 'user', 'content': """Follow this json schema: 
-            {
-                score:float,
-                feedback:str,
-                report_id:str
-            }"""},
-        {'role': 'user',
-         'content': "Score is a percentage of 0-100 represented by a float (i.e 0.5 for 50% and 1.0 for 100%"},
-        {'role': 'user', 'content': "Report id is a short string representation (i.e val_01_report)"}
-    ]
+    with open('art_grading_instructions', 'r') as file:
+        file_data = file.read()
 
-    prompt = personality + messages
+    file_data = file_data.replace('{assignment}', assignment)
 
+    print(file_data)
+
+    instructions = {'role': 'user', 'content': file_data, 'images': [img_final]}
+
+    prompt = get_personality()
+    prompt.append(instructions)
+
+    # Move chat logic to agent and inject prompt and format
     response = con.client.chat(model, messages=prompt, format=Grade.model_json_schema(), think=True, options=options)
     print(response.message.thinking)
     grade: Grade = Grade.model_validate_json(response.message.content)
@@ -119,82 +147,33 @@ def generate_lesson_plan(topic: str, time_commit : str, skill : str) -> LessonPl
 
     model = 'qwen3-vl:235b-cloud'
 
-    messages = [
-        {'role': 'user', 'content': """Can you generate a lesson plan with three lessons on the following topic, time 
-        commitment, and skill level with clear directions for me to exercise my skills. 
-        """},
-        {'role': 'user', 'content': topic},
-        {'role': 'user', 'content': time_commit},
-        {'role': 'user', 'content': skill},
-        {'role': 'user', 'content': """
-            Lesson Structure:
-                - Introduction: Define the subtopics of the lesson and their importance.
-                - Core Concepts: Key elements of the lesson exercises.
-                - Time Needed
-                - Skill Level
-                - Exercises: A series of progressive exercises with clear instructions, materials needed, and techniques.
-                - Common Pitfalls: What to avoid and how to correct.
-                - Final Note: Up to you. Give encouragement; motivate me to practice and improve. Or you can give examples on art where this topic applies
-        """},
-        {'role': 'user', 'content': """Name the lesson plan after the topic. Give it three lessons each with a title 
-        based on its content, descriptive content based on the lesson structure above, and its order in the lesson plan.
-        """},
-        {'role': 'user', 'content': """Finally, create a assessment for me to demonstrate proficiency in the skills honed
-        by these lessons. It should be more creative than the exercises, more interesting than drills, but manageable
-        for my current skills (Beginner for now). Give the assessment a title, clear directions in its content, and a list
-        of requirements to be met, the point total of each. The points of each req will be added to a total for grading
-        at a later time."""},
-        {'role': 'user', 'content': """Follow this json schema: 
-        {
-            section: str,
-            lessons: [
-                {
-                    title: str,
-                    content: str,
-                    order: int
-                }
-            ],
-            assessment: {
-                title: str,
-                content: str,
-                requirements: [
-                    {
-                        name: str,
-                        points: int,
-                    }
-                ]
-            }
-        }
-        """}
-    ]
+    with open('lesson_plan_instructions', 'r') as file:
+        file_data = file.read()
 
-    prompt = personality + messages
+    file_data = file_data.replace('{topic}', topic)
+    file_data = file_data.replace('{time_commit}', time_commit)
+    file_data = file_data.replace('{skill}', skill)
+
+    print(file_data)
+
+    instructions = {'role': 'user', 'content': file_data}
+
+    prompt = get_personality()
+    prompt.append(instructions)
 
     response = con.client.chat(
         model,
         messages=prompt, format=LessonPlan.model_json_schema(),
-        tools=[get_user_materials],
         think=True, options=options)
     print(response.message.thinking)
-    print(response.message.tool_calls)
-    # Handle Tool Calls
-    info = []
-    for call in response.message.tool_calls:
-        func = tools.get(call.function.name)
-        if func:
-            # Try Catch
-            result = func(**call.function.arguments)
-            info.append({'role': 'tool', 'tool_name': call.function.name, 'content': str(result)})
 
-    prompt += info
+    lesson_plan: LessonPlan = None
 
-    informed_response = con.client.chat(
-        model,
-        messages=prompt, format=LessonPlan.model_json_schema(),
-        tools=[get_user_materials],
-        think=True, options=options)
+    try:
+        lesson_plan: LessonPlan = LessonPlan.model_validate_json(response.message.content)
+    except ValidationError:
+        print("Invalid JSON")
+    except:
+        print("Unknown Exception")
 
-    print(response.message.thinking)
-
-    lesson_plan: LessonPlan = LessonPlan.model_validate_json(informed_response.message.content)
     return lesson_plan
